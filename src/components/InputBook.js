8194957799eb84d075761463d6c0e71b95cc347b
joskalenda <joskalenda3@gmail.com>
2022-02-22 16:25:13 +0200

    Create add book component

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -2,7 +2,16 @@
 const InputBook = () => {
 
   return (
-   
+    <form onSubmit={(e) => e.preventDefault()} className="form-container">
+        <input
+          className="input-text"
+          type="text"
+          placeholder="Add Book..."
+          name="title"
+          onChange={(e) => e.preventDefault()}
+        />
+        <button type="submit" className="input-submit" value="Add Book"/>
+    </form>
   );
 }
  
