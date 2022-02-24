f9a2571919dce328d522589365f6311067982d86
joskalenda <joskalenda3@gmail.com>
2022-02-24 20:33:27 +0200

    Import books reducer

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,5 +1,5 @@
-import { useSelector } from 'react-redux';
-import styles from './book.module.css';
+// import { useSelector } from 'react-redux';
+// import styles from './book.module.css';
 
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
@@ -13,7 +13,7 @@ const initialState = [
   },
 ];
 
-export const Reducer = (state = initialState, action) => {
+export const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state,
@@ -33,7 +33,6 @@ export const Reducer = (state = initialState, action) => {
       return state;
   }
 };
-// export default reducer;
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
@@ -45,52 +44,3 @@ export const removeBook = (payload) => ({
   payload,
 
 });
-
-export const Book = () => {
-  const books = useSelector((state) => {
-    const { books } = state;
-    console.log(books);
-
-    // const dispatch = useDispatch();
-    // const removeBookFromStore = () => {
-    //   dispatch(removeBook());
-    // };
-    books.map((book) => (
-      <div className={styles.listBook} key={book.id}>
-        <div className={styles.divOne}>
-          <span className={styles.genre}>{book.genre}</span>
-          <h2 className={styles.title}>{book.title}</h2>
-          <span className={styles.author}>{book.author}</span>
-          <div className={styles.removeButton}>
-            <button type="button">Comment</button>
-            <button type="button">Remove</button>
-            <button type="button">Edit</button>
-          </div>
-        </div>
-        <div>
-          <p>{book.completed}</p>
-        </div>
-        <div className={styles.divOne}>
-          <h3>CURRENT CHAPTER</h3>
-          <p>Chapter 3</p>
-          <button type="button">Update progress</button>
-        </div>
-      </div>
-    ));
-  });
-
-  const para = {
-    marginTop: '30px',
-
-  };
-
-  return (
-    <div>
-      <div style={para}>{books}</div>
-      {/* <InputBook /> */}
-    </div>
-
-  );
-};
-
-// export default Book;
