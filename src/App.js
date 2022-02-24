70185ef8747a30485aebde6ddc6a6fb8adc73a68
joskalenda <joskalenda3@gmail.com>
2022-02-24 20:17:50 +0200

    Import homepage to the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavLink from './components/NavLink';
-import { Book } from './redux/books/book';
-import InputBook from './components/InputBook';
+import Homepage from './components/Homepage';
 import Categories from './redux/categories/categories';
 
 function App() {
@@ -9,9 +8,8 @@ function App() {
     <BrowserRouter>
       <NavLink />
       <Routes>
-        <Route path="/" element={<Book />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/Categories" element={<Categories />} />
-        <Route path="/form" element={<InputBook />} />
       </Routes>
     </BrowserRouter>
   );
