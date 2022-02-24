67ff4ca1d224f5e0afac4b4232b4b32be0e2810b
joskalenda <joskalenda3@gmail.com>
2022-02-24 18:11:17 +0200

    Create add and remove functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavLink from './components/NavLink';
-import Book from './redux/books/book';
+import { Book } from './redux/books/book';
+import InputBook from './components/InputBook';
 import Categories from './redux/categories/categories';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Book />} />
         <Route path="/Categories" element={<Categories />} />
+        <Route path="/form" element={<InputBook />} />
       </Routes>
     </BrowserRouter>
   );
--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,51 +1,84 @@
+import { useSelector } from 'react-redux';
 import styles from './book.module.css';
-import InputBook from '../../components/InputBook';
 
-const Book = () => {
-  const books = [
-    {
-      id: 1,
-      title: 'Ulsan',
-      author: 'Thomas Hunk',
-      genre: 'Music',
-      completed: 10,
-    },
-    {
-      title: 'Mucks',
-      author: 'King Robert',
-      genre: 'Science Fiction',
-      completed: 8,
-      id: 1,
-    },
-    {
-      title: 'Koran',
-      author: 'Bullet smoke',
-      genre: 'Magic',
-      completed: 8,
-      id: 1,
-    },
-  ].map((book) => (
-    <div className={styles.listBook} key={book.id} completed={book.completed}>
-      <div className={styles.divOne1}>
-        <span className={styles.genre}>{book.genre}</span>
-        <h2 className={styles.title}>{book.title}</h2>
-        <span className={styles.author}>{book.author}</span>
-        <div className={styles.removeButton}>
-          <button type="button">Comment</button>
-          <button type="button">Remove</button>
-          <button type="button">Edit</button>
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const initialState = [
+  {
+    id: 1,
+    title: 'Ulsan',
+    author: 'Thomas Hunk',
+    genre: 'Music',
+    completed: 10,
+  },
+];
+
+export const Reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state,
+        {
+          id: 9,
+          title: action.payload.title,
+          author: action.payload.author,
+          genre: action.payload.genre,
+          completed: 90,
+        },
+      ];
+
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+
+    default:
+      return state;
+  }
+};
+// export default reducer;
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+
+});
+
+export const Book = () => {
+  const books = useSelector((state) => {
+    const { books } = state;
+    console.log(books);
+
+    // const dispatch = useDispatch();
+    // const removeBookFromStore = () => {
+    //   dispatch(removeBook());
+    // };
+    books.map((book) => (
+      <div className={styles.listBook} key={book.id}>
+        <div className={styles.divOne}>
+          <span className={styles.genre}>{book.genre}</span>
+          <h2 className={styles.title}>{book.title}</h2>
+          <span className={styles.author}>{book.author}</span>
+          <div className={styles.removeButton}>
+            <button type="button">Comment</button>
+            <button type="button">Remove</button>
+            <button type="button">Edit</button>
+          </div>
+        </div>
+        <div>
+          <p>{book.completed}</p>
+        </div>
+        <div className={styles.divOne}>
+          <h3>CURRENT CHAPTER</h3>
+          <p>Chapter 3</p>
+          <button type="button">Update progress</button>
         </div>
       </div>
-      <div>
-        <p>50% progress</p>
-      </div>
-      <div className={styles.divOne}>
-        <h3>CURRENT CHAPTER</h3>
-        <p>Chapter 3</p>
-        <button type="button">Update progress</button>
-      </div>
-    </div>
-  ));
+    ));
+  });
+
   const para = {
     marginTop: '30px',
 
@@ -54,10 +87,10 @@ const Book = () => {
   return (
     <div>
       <div style={para}>{books}</div>
-      <InputBook />
+      {/* <InputBook /> */}
     </div>
 
   );
 };
 
-export default Book;
+// export default Book;
