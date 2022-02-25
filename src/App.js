3662f4c7249fb88de82e658298f27dd2b364f813
joskalenda <joskalenda3@gmail.com>
2022-02-25 14:12:51 +0200

    Fix file extension issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-key */
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavLink from './components/NavLink';
+// eslint-disable-next-line import/extensions
 import Homepage from './components/Homepage';
 import Categories from './redux/categories/categories';
 
